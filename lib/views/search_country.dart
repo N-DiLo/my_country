974e9426a4fcbbd04829173442018d896f513c74
N-DiLo <egbekwunwanedilobu@gmail.com>
2022-11-13 13:19:37 +0100

    update README and fixed app colour modes

--- lib/views/search_country.dart
+++ lib/views/search_country.dart
@@ -7,6 +7,7 @@ import 'package:my_country/services/web_call.dart';
 import 'package:my_country/utils/responsive.dart';
 import 'package:my_country/utils/app_container.dart';
 import 'package:my_country/views/country_details.dart';
+import 'package:provider/provider.dart';
 
 class SearchCountry extends StatefulWidget {
   const SearchCountry({super.key});
@@ -37,72 +38,80 @@ class _SearchCountryState extends State<SearchCountry> {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenLayout(
-      mobile: Scaffold(
-        appBar: AppBar(
-          actions: [
-            Padding(
-              padding: REdgeInsets.only(right: 24.0),
-              child: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.light_mode_outlined,
-                  color: AppColor.grayWarm,
+    return Consumer(
+      builder:
+          (BuildContext context, ThemeChanger themeChanger, Widget? child) =>
+              ScreenLayout(
+        mobile: Scaffold(
+          appBar: AppBar(
+            actions: [
+              Padding(
+                padding: REdgeInsets.only(right: 24.0),
+                child: IconButton(
+                  onPressed: () {
+                    themeChanger.isDarkTheme = !themeChanger.isDarkTheme;
+                  },
+                  icon: Icon(
+                    themeChanger.isDarkTheme
+                        ? Icons.light_mode_outlined
+                        : Icons.dark_mode_rounded,
+                    color: AppColor.grayWarm,
+                  ),
                 ),
               ),
-            ),
-          ],
-          elevation: 0,
-          backgroundColor: AppColor.whiteColor,
-          title: RichText(
-            text: TextSpan(
-                text: 'Explore',
-                style: myAppBarText,
-                children: [TextSpan(text: '.', style: logoDot)]),
-          ),
-        ),
-        backgroundColor: AppColor.whiteColor,
-        body: Padding(
-          padding: REdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SearchBar(),
-              SizedBox(height: ScreenUtil().setHeight(16)),
-              const SelectZone(),
-              SizedBox(height: ScreenUtil().setHeight(16)),
-              Expanded(
-                  child: MediaQuery.removePadding(
-                context: context,
-                child: Visibility(
-                  visible: isLoaded,
-                  replacement: const Center(child: CircularProgressIndicator()),
-                  child: ListView.builder(
-                      padding: REdgeInsets.symmetric(vertical: 26),
-                      itemCount: explore?.length,
-                      itemBuilder: ((context, index) => ListTile(
-                            onTap: () => Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (_) => CountryDetials(
-                                        details: explore![index]))),
-                            leading: Container(
-                                width: ScreenUtil().setWidth(40),
-                                height: ScreenUtil().setHeight(40),
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(4),
-                                    image: DecorationImage(
-                                        fit: BoxFit.fill,
-                                        image: NetworkImage(
-                                            explore![index].flags!.png!)))),
-                            title: Text(explore![index].name!.common!,
-                                style: homelist),
-                            subtitle: Text(explore![index].capital!.first,
-                                style: sublist),
-                          ))),
-                ),
-              ))
             ],
+            elevation: 0,
+            title: RichText(
+              text: TextSpan(
+                  text: 'Explore',
+                  style: myAppBarText,
+                  children: [TextSpan(text: '.', style: logoDot)]),
+            ),
+          ),
+          body: Padding(
+            padding: REdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: ScreenUtil().setHeight(10)),
+                const SearchBar(),
+                SizedBox(height: ScreenUtil().setHeight(16)),
+                const SelectZone(),
+                SizedBox(height: ScreenUtil().setHeight(16)),
+                Expanded(
+                    child: MediaQuery.removePadding(
+                  context: context,
+                  child: Visibility(
+                    visible: isLoaded,
+                    replacement:
+                        const Center(child: CircularProgressIndicator()),
+                    child: ListView.builder(
+                        padding: REdgeInsets.symmetric(vertical: 26),
+                        itemCount: explore?.length,
+                        itemBuilder: ((context, index) => ListTile(
+                              onTap: () => Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (_) => CountryDetials(
+                                          details: explore![index]))),
+                              leading: Container(
+                                  width: ScreenUtil().setWidth(40),
+                                  height: ScreenUtil().setHeight(40),
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(4),
+                                      image: DecorationImage(
+                                          fit: BoxFit.fill,
+                                          image: NetworkImage(
+                                              explore![index].flags!.png!)))),
+                              title: Text(explore![index].name!.common!,
+                                  style: homelist),
+                              subtitle: Text(explore![index].capital!.first,
+                                  style: sublist),
+                            ))),
+                  ),
+                ))
+              ],
+            ),
           ),
         ),
       ),
